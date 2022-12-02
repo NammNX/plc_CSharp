51edb369cbc47d9be8fa7b1d2229b921527cf0cf
NammNX <114397963+NammNX@users.noreply.github.com>
2022-12-02 16:35:43 +0700

    first commit

---
+++ PLC_CSharp/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace PLC_CSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.BtnDisconnect = new System.Windows.Forms.Button();
+            this.BtnTest = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(243, 32);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // BtnDisconnect
+            // 
+            this.BtnDisconnect.Location = new System.Drawing.Point(243, 88);
+            this.BtnDisconnect.Name = "BtnDisconnect";
+            this.BtnDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.BtnDisconnect.TabIndex = 1;
+            this.BtnDisconnect.Text = "Disconnect";
+            this.BtnDisconnect.UseVisualStyleBackColor = true;
+            this.BtnDisconnect.Click += new System.EventHandler(this.BtnDisconnect_Click);
+            // 
+            // BtnTest
+            // 
+            this.BtnTest.Location = new System.Drawing.Point(243, 169);
+            this.BtnTest.Name = "BtnTest";
+            this.BtnTest.Size = new System.Drawing.Size(75, 23);
+            this.BtnTest.TabIndex = 2;
+            this.BtnTest.Text = "Test";
+            this.BtnTest.UseVisualStyleBackColor = true;
+            this.BtnTest.Click += new System.EventHandler(this.BtnTest_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(457, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(457, 202);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label1";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(457, 233);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label1";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnTest);
+            this.Controls.Add(this.BtnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button BtnDisconnect;
+        private System.Windows.Forms.Button BtnTest;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ PLC_CSharp/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HslCommunication.Profinet.Melsec;
+using HslCommunication;
+
+
+namespace PLC_CSharp
+{
+
+    public partial class Form1 : Form
+    {
+        MelsecFxSerial melsecFx = new MelsecFxSerial();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            melsecFx.SerialPortInni(sp =>
+            {
+                sp.PortName = "COM4";
+                sp.BaudRate = 38400;
+                sp.DataBits = 7;
+                sp.StopBits = System.IO.Ports.StopBits.One;
+                sp.Parity = System.IO.Ports.Parity.Even;
+            });
+
+        }
+
+          
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+
+            
+            
+            melsecFx.Open();
+            bool b = melsecFx.IsOpen();
+            
+
+
+
+            Console.WriteLine($"isopen = {b}"); 
+        }
+
+        private void BtnDisconnect_Click(object sender, EventArgs e)
+        {
+            melsecFx.Close();
+        }
+
+        private void BtnTest_Click(object sender, EventArgs e)
+        {
+
+            bool b = melsecFx.IsOpen();
+
+            Console.WriteLine($"isopen = {b}");
+
+            short short_D100 = melsecFx.ReadInt16("D100").Content;   
+
+            Console.WriteLine($"D100 = {short_D100}");
+
+            bool X1 = melsecFx.ReadBool("X1").Content;
+
+            Console.WriteLine($"X1 = {X1}");
+
+            bool M500 = melsecFx.ReadBool("M500").Content;
+
+            Console.WriteLine($"M500 = {M500}");
+            bool Y0 = melsecFx.ReadBool("Y0").Content;
+
+            Console.WriteLine($"Y0 = {Y0}");
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            
+
+        }
+
+        
+    }
+}
